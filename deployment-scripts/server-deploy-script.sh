6185e370ed15f4b7de45e099816cafc3a7860b10
gbenhaim <galbh2@gmail.com>
2016-04-04 20:17:20 +0300

    Created deployment script for the Jenkins server. Changed the type of os installed on the vms.

--- deployment-scripts/server-deploy-script.sh
+++ deployment-scripts/server-deploy-script.sh
@@ -1,2 +1,26 @@
+
 #!/bin/bash
-echo 'host0' > ~/server.test
+yum install -y java-1.8.0-openjdk-devel.x86_64 \
+  wget
+
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+rpm --import http://pkg.jenkins-ci.org/redhat-stable/jenkins-ci.org.key
+
+yum install -y jenkins
+
+# firewall configuration
+
+firewall-cmd --zone=public --add-port=8080/tcp --permanent
+
+firewall-cmd --zone=public --add-service=http --permanent
+
+firewall-cmd --reload
+
+# start jenkins service
+
+service jenkins start
+
+chkconfig jenkins on
+
+# additional tools
+#yum install net-tools.x86_64
