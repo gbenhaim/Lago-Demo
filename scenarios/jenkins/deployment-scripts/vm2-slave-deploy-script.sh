f8c2ffc62d13526ab0b2ecb0aaaccacbf6b7dfdd
gbenhaim <galbh2@gmail.com>
2016-05-16 17:21:54 +0300

    lago demo

--- scenarios/jenkins/deployment-scripts/vm2-slave-deploy-script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -xe
-yum install -y java-1.8.0-openjdk-devel.x86_64 \
-  net-tools.x86_64
-
-# As this is the same as vm1, you can use the same script, or soft-link from
-# one to the other
